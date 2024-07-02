3eb312b750945ec999a0daf37364a52473528d29
hiki7 <rsaliyev03@gmail.com>
2024-07-02 21:47:46 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+API_KEY = 'bd5e378503939ddaee76f12ad7a97608'
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+
+@app.get("/weather")
+def get_weather(city: str):
+    response = requests.get(f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric")
+
+    data = response.json()
+
+    return {
+        "city": data['name'],
+        "temperature": data['main']['temp']
+    }
+
+
